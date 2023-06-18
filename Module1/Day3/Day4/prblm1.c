5ef6bcff97065b938bc08797243e5006c1cf6755
JyothiPadmavathiGeddada <jyothi.g.padmaja@gmail.com>
2023-06-18 11:03:23 +0530

    Day4

---
+++ Module1/Day3/Day4/prblm1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {12, 84, 20, 36, 64, 56, 72, 10, 45, 90};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+    float avg;
+
+    // Calculating the sum
+    for (int i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+
+    // Calculating the average
+    avg = (float)sum / len;
+
+    printf("Sum: %d\n", sum);
+    printf("Average: %.2f\n", avg);
+
+    return 0;
+}
